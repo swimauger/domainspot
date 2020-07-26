532e486434394816b2b7590b1a1a57db4470d808
swimauger <swimauger@gmail.com>
2020-07-26 13:53:33 -0500

    Add queue to rest requests

--- rest.js
+++ rest.js
@@ -8,18 +8,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.post('/', async (req, res) => {
-    const { domain, extensions } = req.body;
-    if (!domain) {
-        res.send(JSON.stringify({
-            error: "No Domain Specified"
-        }));
-    } else {
-        res.send(JSON.stringify(
-            await lookup(domain, extensions)
-        ));
+class Queue {
+    static tasks = [];
+
+    static add(req, res) {
+        this.tasks.push({ req, res });
+        if (this.tasks.length < 2) {
+            this.scrape();
+        }
     }
-});
+
+    static async scrape() {
+        for (const task of this.tasks) {
+            const { domain, extensions } = task.req.body;
+            if (!domain) {
+                task.res.send(JSON.stringify({
+                    error: "No Domain Specified"
+                }));
+            } else {
+                task.res.send(JSON.stringify(
+                    await lookup(domain, extensions)
+                ));
+            }
+        }
+    }
+}
+
+app.post('/', Queue.add.bind(Queue));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
